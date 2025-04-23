e1de2347c7934517170e3e86fa073388c3db3f9e
isa777 <consal135@gmail.com>
2025-04-22 19:37:49 -0600

    feat: adding simulated CIDs for the rabbits NFTs

--- test/Rabbits721.t.sol
+++ test/Rabbits721.t.sol
@@ -7,14 +7,27 @@ import {Rabbits} from "../src/Rabbits721.sol";
 contract RabbitsTest is Test {
     Rabbits public rabbits;
     string public baseURI;
+    string[] public NFTs;
 
     function setUp() public {
         baseURI = "https://gateway.decentralizedscience.org/ipfs/";
         rabbits = new Rabbits(address(this), address(this));
+        NFTs = [
+            "bafybeigdyzwa5ez6mrn3yk97kcrf4qkuuygwgxv3wqhxqjx6m72nzlvpea",
+            "bafybeid3d4e3c4a5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f6",
+            "bafybeif4e3d4a5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f6g7",
+            "bafybeigdyzwa5ez6mrn3yk97kcrf4qkuuygwgxv3wqhxqjx6m72nzlvpea",
+            "bafybeid3d4e3c4a5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f6",
+            "bafybeif4e3d4a5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f6g7",
+            "bafybeigdyzwa5ez6mrn3yk97kcrf4qkuuygwgxv3wqhxqjx6m72nzlvpea",
+            "bafybeid3d4e3c4a5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f6",
+            "bafybeif4e3d4a5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f6g7",
+            "bafybeigdyzwa5ez6mrn3yk97kcrf4qkuuygwgxv3wqhxqjx6m72nzlvpea"
+        ];
     }
 
     function testMint() public {
-        rabbits.safeMint(address(0x1), "abc");
+        rabbits.safeMint(address(0x1), NFTs[0]);
         assertEq(rabbits.ownerOf(0), address(0x1),
                  "Token should be minted to specified address.");
     }
